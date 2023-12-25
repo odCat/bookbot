3518414ae6fc37cf162bfb01e38337618e84f801
Mihai Gătejescu <gus666xe@gmail.com>
2023-12-25 22:02:17 +0200

    Count words and letters

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def count_letters(text):
+    result = {}
+    for letter in text.lower():
+        if letter not in result:
+            result[letter] = 1
+        else:
+            result[letter] += 1
+    return result
+
+
 with open('books/frankenstein.txt') as book:
     content = book.read()
-    print(content)
+    print(count_letters(content))
+    words = content.split()
+    print(len(words))
